194ffe14f6622beddf4b4b65a80f43658e707bfb
4byte <medhigh.medhigh@gmail.com>
2018-11-14 14:47:33 +0200

    build runs with amqp update for 1 lab 3 paragraph

--- hello-service/src/main/java/com/ex/microservices/lab/helloservice/HelloServiceApplication.java
+++ hello-service/src/main/java/com/ex/microservices/lab/helloservice/HelloServiceApplication.java
@@ -1,8 +1,11 @@
 package com.ex.microservices.lab.helloservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+@RefreshScope
 @SpringBootApplication
 public class HelloServiceApplication {
 
--- hello-service/src/main/java/com/ex/microservices/lab/helloservice/controller/HelloController.java
+++ hello-service/src/main/java/com/ex/microservices/lab/helloservice/controller/HelloController.java
@@ -1,11 +1,13 @@
 package com.ex.microservices.lab.helloservice.controller;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@RefreshScope
 @Component
 @Controller
 @RequestMapping("/")
