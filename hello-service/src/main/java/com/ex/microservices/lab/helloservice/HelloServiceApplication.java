e16da04cfeb7ecd7caee40ea675f0bad25502dae
4byte <medhigh.medhigh@gmail.com>
2018-11-16 15:11:50 +0200

    added eureka clients pom and url to hello and config services

--- hello-service/src/main/java/com/ex/microservices/lab/helloservice/HelloServiceApplication.java
+++ hello-service/src/main/java/com/ex/microservices/lab/helloservice/HelloServiceApplication.java
@@ -1,11 +1,10 @@
 package com.ex.microservices.lab.helloservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HelloServiceApplication {
 
