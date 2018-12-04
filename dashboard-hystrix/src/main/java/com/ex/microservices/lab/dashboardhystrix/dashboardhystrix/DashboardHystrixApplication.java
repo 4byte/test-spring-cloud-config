fb8b5354bf7b454a7438067444ac515eaa434253
4byte <medhigh.medhigh@gmail.com>
2018-12-05 00:43:34 +0200

    hystrix dashboard template added

---
+++ dashboard-hystrix/src/main/java/com/ex/microservices/lab/dashboardhystrix/dashboardhystrix/DashboardHystrixApplication.java
@@ -0,0 +1,15 @@
+package com.ex.microservices.lab.dashboardhystrix.dashboardhystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableHystrixDashboard
+@EnableEurekaClient
+@SpringBootApplication
+public class DashboardHystrixApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardHystrixApplication.class, args);
+	}
+}
